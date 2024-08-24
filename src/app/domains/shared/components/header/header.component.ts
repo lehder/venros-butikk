7808b9330a58554678d871df62ace108b793800c
Lehder Harold <lehderjhoyy38@gmail.com>
2024-08-24 14:15:59 +0200

    Agragando carpetas con nuevas imagenes

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
 import { RouterLinkWithHref } from '@angular/router';
@@ -14,10 +14,22 @@ import { AboutComponent } from '../../../info/pages/about/about.component';
 export class HeaderComponent {
   hideSideMenu = signal(true);
   @Input({required: true}) cart: Product[] = []; 
+  total = signal(0);
   
 
   toogleSideMenu() { 
   this.hideSideMenu.update(prevState => !prevState);
+ }
+
+ ngOnChanges(changes: SimpleChanges) {
+  const cart = changes['cart'];
+  if (cart) {
+    this.total.set(this.calcTotal());
+  }
+ }
+
+ calcTotal() {
+  return this.cart.reduce((total, product) => total +  product.price, 0);
  }
 
 }
